61bde1211386dfb70770f94471cc43ea010f15d7
Derryck <oseiagyemanb@gmail.com>
2022-10-16 13:09:46 +0100

    update of file

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
+//Initializing the Game on page Load
 
+initCategoryRow() //we call the function
+initBoard()  //we call the function 
+
+function initCategoryRow(){
+    let categoryRow = document.getElementById('category-row')
+
+    for (let i = 0; i < 6; i++){
+        let box = document.createElement('div') // This will create the box
+        box.className = 'clue-box category-box' //We gave our box a className of 'clue-box' and 'category-box'
+        categoryRow.appendChild(box) //we nest the box inside the categoryRow(like putting each box in the row it will be sitting )
+    }
+
+}
+
+function initBoard(){
+    let board = document.getElementById('clue-board')
+
+    //We generate 5 rows, then place 6 boxes in each row
+
+    for (let i = 0; i < 5; i++){ //Creating of the rows (outer loop)
+        let row = document.createElement('div') //we created a div tag so we assign it to row
+        let boxValue = 200 * (i + 1) //This will help increment each row by a dollar value
+        row.className = 'clue-row' //We gave our row a className of 'clue-row'
+
+        for (let j = 0; j < 6; j++){ //Creating of the 6 boxes in each row(inner loop)
+            let box = document.createElement('div')// This will create the box
+            box.className = 'clue-box' //We gave our box a className of 'clue-box'
+            box.textContent = '$' + boxValue //we will add the boxValue to the inside of the box
+            box.addEventListener('click',getclue,false) //we added an addEventListener to the box
+            row.appendChild(box) //This will nest the box inside the row(like putting each box in the row it will be sitting )
+        }
+
+        board.appendChild(row) //we need to attach each row we created in the game board itself(so it can be seen)
+    }
+}
+
+function getclue(){
+    console.log('Just getting started')
+}
